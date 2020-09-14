2d6bd022b3bf1b1203a9978997d65272ba3718f7
Nathaniel Essick <nlessick@dmacc.edu>
2020-09-14 12:51:34 -0500

    Included Project and Unit Test

---
+++ FirstResponsiveWebAppEssick/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstResponsiveWebAppEssick.Models;
+using Microsoft.Win32;
+using System;
+
+namespace FirstResponsiveWebAppEssick.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.BY = "0";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(ResponsiveAppModel model)
+        {
+            ViewBag.BY = model.AgeThisYear();
+            return View(model);
+        }
+    }
+}
---
+++ FirstResponsiveWebAppEssick/Models/ResponsiveAppModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstResponsiveWebAppEssick.Models
+{
+    public class ResponsiveAppModel
+    {
+        public string UserName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public string AgeThisYear()
+        {
+            
+            if (DateOfBirth > DateTime.Now)
+                return UserName + ", you entered a date in the future. Program will now terminate.";
+
+            int years = DateTime.Now.Year - DateOfBirth.Year;
+
+            if (DateTime.Now.Month < DateOfBirth.Month || DateTime.Now.Month == DateOfBirth.Month && DateTime.Now.Day < DateOfBirth.Day)
+            { years--; }
+
+            if (years >= 0)
+            {
+                return "Hello " + UserName+ ". You are " + years + " years old.";
+            }
+            else
+            { return "Hello " + UserName + ". You are zero years old."; }
+        }
+    }
+}
